79d759233c17de1e33316bb640a830bc6e62e191
Martin Vladic <martin.vladic@gmail.com>
2024-04-09 10:03:54 +0200

    Inital commit

---
+++ src/ui/images.c
@@ -0,0 +1,6 @@
+#include "images.h"
+
+const ext_img_desc_t images[2] = {
+    { "log_eez", &img_log_eez },
+    { "logo_lvgl", &img_logo_lvgl },
+};
---
+++ src/ui/screens.h
@@ -0,0 +1,30 @@
+#ifndef EEZ_LVGL_UI_SCREENS_H
+#define EEZ_LVGL_UI_SCREENS_H
+
+#include <lvgl/lvgl.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct _objects_t {
+    lv_obj_t *Main;
+    lv_obj_t *_obj_button_1;
+    lv_obj_t *_obj_button_2;
+    lv_obj_t *_obj_label_0;
+} objects_t;
+
+extern objects_t objects;
+
+void create_screen_main();
+void tick_screen_main();
+
+void create_screens();
+void tick_screen(int screen_index);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*EEZ_LVGL_UI_SCREENS_H*/
